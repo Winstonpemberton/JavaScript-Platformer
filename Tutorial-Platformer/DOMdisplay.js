b24ff8794f0d9753d0d1e53e1b03150351fab819
Winstonpemberton <winstonpemberton@gmail.com>
2021-02-13 10:02:56 -0500

    added camera scrolling and a html page for displaying the game

--- Tutorial-Platformer/DOMdisplay.js
+++ Tutorial-Platformer/DOMdisplay.js
@@ -73,6 +73,30 @@ DOMdisplay.prototype.syncState = function(state) {
     // the current object also uses a method called scrollPlayerIntoView method with the current state passed into it
     this.scrollPlayerIntoView(state)
 }
+
+DOMdisplay.prototype.scrollPlayerIntoView = function (state) {
+    let width = this.dom.clientWidth
+    let height = this.dom.clientHeight
+    let margin = width/3 
+
+    let left = this.dom.scrollLeft, right = left + width 
+    let top = this.dom.scrollTop,bottom = top + height 
+
+    let player = state.player
+    let center = player.pos.plus(player.size.times(0.5)).times(scale)
+
+    if (center.x < left + margin){
+        this.dom.scrollLeft = center.x - margin
+    }else if (center.x > right - margin){
+        this.dom.scrollLeft = center.x + margin - width 
+    }
+    if (center.y <top + margin){
+        this.dom.scrollTop = center.y - margin
+    }else if (center.y > bottom - margin){
+        this.dom.scrollTop = center.y + margin - height
+    }
+    
+}
     
 
 
