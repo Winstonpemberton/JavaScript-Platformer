ee1fabfea4f03528fb504deb95ddd4d10856971d
Winstonpemberton <winstonpemberton@gmail.com>
2021-03-09 18:45:57 -0500

    added code for creating coin actors

--- Platformer-frontend/coin.js
+++ Platformer-frontend/coin.js
@@ -1,3 +1,14 @@
 class Coin{
+    constructor(position){
+        this.position = position
+    }
+
+    get type(){
+        return "coin"
+    }
+
+    static create(position){
+        new Coin(position)
+    }
     
 }
